16203f5011960ab0ecaabd1fe61aac879e30cfe0
Aarthi.S <168005532+Aarthi-2004@users.noreply.github.com>
2025-01-28 20:01:32 +0530

    Create Arithmetic Operations.py

---
+++ Arithmetic Operations.py
@@ -0,0 +1,25 @@
+Create Arithmetic Operations.pyTask
+The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+
+The first line contains the sum of the two numbers.
+The second line contains the difference of the two numbers (first - second).
+The third line contains the product of the two numbers.
+
+Example
+Print the following:
+8
+-2
+15
+
+PYTHON CODE:
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    # Calculate and print the sum
+    print(a + b)
+    
+    # Calculate and print the difference
+    print(a - b)
+    
+    # Calculate and print the product
+    print(a * b)
